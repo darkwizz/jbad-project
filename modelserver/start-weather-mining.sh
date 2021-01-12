ed30fc0dd71d4830d7f13fd72b4d8c61460c2aa6
Artur Sokol <psyhoge@gmail.com>
2021-01-12 23:31:19 +0100

    prepare a script for starting weather mining program on scheduler

---
+++ modelserver/start-weather-mining.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MINING_APP_PATH=$( realpath weathergrabber.py )
+PYTHON_PATH=$( which python )
+VARS_SH_PATH=$( realpath init-vars.sh )
+CMD_PATH=$( realpath mine-weather.sh )
+
+echo $MINING_APP_PATH
+echo $PYTHON_PATH
+echo $VARS_SH_PATH
+echo $CMD_PATH
+
+TEMP_CRON=mycron
+crontab -u $USER -l > $TEMP_CRON
+echo "*/30 * * * * $CMD_PATH $VARS_SH_PATH $PYTHON_PATH $MINING_APP_PATH" >> $TEMP_CRON
+# crontab -u $USER $TEMP_CRON
+rm $TEMP_CRON
