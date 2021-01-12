0a203239cd759c01f5e662a305e33bf42b42a7da
Artur Sokol <psyhoge@gmail.com>
2021-01-12 23:43:43 +0100

    implement min-weather.sh, comment crontab parts in start-weather-mining.sh temporarily

--- modelserver/mine-weather.sh
+++ modelserver/mine-weather.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+if [[ $# -lt 3 ]] ; then
+	echo "Not enough parameters"
+	exit
+fi
+
+VARS_SH=$1
+PYTHON_PATH=$2
+APP_PATH=$3
+
+source $VARS_SH
+
+$PYTHON_PATH $APP_PATH
--- modelserver/start-weather-mining.sh
+++ modelserver/start-weather-mining.sh
@@ -2,16 +2,17 @@
 
 MINING_APP_PATH=$( realpath weathergrabber.py )
 PYTHON_PATH=$( which python )
-VARS_SH_PATH=$( realpath init-vars.sh )
 CMD_PATH=$( realpath mine-weather.sh )
+VARS_SH_PATH=$( realpath init-vars.sh )
 
-echo $MINING_APP_PATH
-echo $PYTHON_PATH
-echo $VARS_SH_PATH
-echo $CMD_PATH
+# echo $MINING_APP_PATH
+# echo $PYTHON_PATH
+# echo $VARS_SH_PATH
+# echo $CMD_PATH
 
 TEMP_CRON=mycron
 crontab -u $USER -l > $TEMP_CRON
-echo "*/30 * * * * $CMD_PATH $VARS_SH_PATH $PYTHON_PATH $MINING_APP_PATH" >> $TEMP_CRON
+$CMD_PATH $VARS_SH_PATH $PYTHON_PATH $MINING_APP_PATH
+# echo "*/30 * * * * $CMD_PATH $API_KEY $PYTHON_PATH $MINING_APP_PATH" >> $TEMP_CRON
 # crontab -u $USER $TEMP_CRON
 rm $TEMP_CRON
