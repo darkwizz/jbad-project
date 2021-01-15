c945c6aee197bf7de9ae4704da93dc7beaf74546
Artur Sokol <psyhoge@gmail.com>
2021-01-15 01:54:46 +0100

    implement /weather/current and /weather/history for model server, update start-test-server.sh with ignoring Redis if not running

--- modelserver/server.py
+++ modelserver/server.py
@@ -1,10 +1,44 @@
-from flask import Flask, jsonify
+from dbaccess.jsonadapter import get_json_db_adapter
+
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+from datetime import date
+
 
 app = Flask(__name__)
 
+@app.route('/weather/history/current')
+def get_current_weather_data():
+    adapter = get_json_db_adapter()
+    result = adapter.get_current_weather()
+    return jsonify(result)
+
+
 @app.route('/weather/history')
-def get_historical_weather_data():
-    return jsonify([])
+def get_historical_weather_data_for_dates():
+    try:
+        start_date = request.args.get(key='start_date', default=None, type=date.fromisoformat)
+        end_date = request.args.get(key='end_date', default=None, type=date.fromisoformat)
+    except ValueError:
+        return {'message': 'Incorrect dates format'}, HTTPStatus.BAD_REQUEST
+    
+    if start_date is None and end_date is None:
+        return {'message': 'Neither <start_date> nor <end_date> are passed'}, HTTPStatus.BAD_REQUEST
+    
+    start_date = start_date or end_date
+    end_date = end_date or date.today()
+
+    try:
+        adapter = get_json_db_adapter()
+    except ValueError as ex:
+        app.logger.error(str(ex))
+        return {'message': 'Env configuration problem'}, HTTPStatus.INTERNAL_SERVER_ERROR
+    
+    try:
+        result = adapter.get_weather_for_period(start_date, end_date)
+        return jsonify(result)
+    except ValueError as ex:
+        return {'message': str(ex)}, HTTPStatus.BAD_REQUEST
 
 
 @app.route('/weather/forecast')
--- modelserver/start-test-server.sh
+++ modelserver/start-test-server.sh
@@ -8,25 +8,32 @@ if [[ -z $CITY_ID || -z $CITY_NAME || -z $MODEL_SERVER_URL ]] ; then
 fi
 
 
-SERVER_KEY="city_$CITY_ID"
-redis-cli del $SERVER_KEY
-redis-cli hset $SERVER_KEY name $CITY_NAME url $MODEL_SERVER_URL id $CITY_ID
+if [[ `redis-cli ping 2> /dev/null` ]] ; then
+	SERVER_KEY="city_$CITY_ID"
+	redis-cli del $SERVER_KEY
+	redis-cli hset $SERVER_KEY name $CITY_NAME url $MODEL_SERVER_URL id $CITY_ID
+	
+	# CRON_ALLOW=/etc/cron.allow
+	# echo $USER > $CRON_ALLOW
 
-# CRON_ALLOW=/etc/cron.allow
-# echo $USER > $CRON_ALLOW
-
-# set key refresh in crontab
-TEMP_CRON=mycron
-crontab -u $USER -l > $TEMP_CRON
-REDIS_CLI=$( which redis-cli )
-CMD_PATH=$( realpath server-checkpoint.sh )
-# echo $CMD_PATH
-echo "*/$REFRESH_TIME_MINUTES * * * * $CMD_PATH $CITY_ID $EXPIRE_TIME_MINUTES $REDIS_CLI" > $TEMP_CRON
-crontab -u $USER $TEMP_CRON
-rm $TEMP_CRON
-# end
+	# set key refresh in crontab
+	TEMP_CRON=mycron
+	ORIG_CRON=origcron
+	crontab -u $USER -l > $TEMP_CRON
+	cp $TEMP_CRON $ORIG_CRON
+	REDIS_CLI=$( which redis-cli )
+	CMD_PATH=$( realpath server-checkpoint.sh )
+	# echo $CMD_PATH
+	echo "*/$REFRESH_TIME_MINUTES * * * * $CMD_PATH $CITY_ID $EXPIRE_TIME_MINUTES $REDIS_CLI" > $TEMP_CRON
+	crontab -u $USER $TEMP_CRON
+	# rm $TEMP_CRON
+	# end
+fi
 
 
 flask run -p 7722
-crontab -u $USER -r
-redis-cli del $SERVER_KEY
+
+if [[ -n $ORIG_CRON ]] ; then
+	crontab -u $USER $ORIG_CRON
+	redis-cli del $SERVER_KEY
+fi
