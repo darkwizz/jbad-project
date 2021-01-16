9c127bc26c19670b74d9368d1cd135ec19e4109b
Artur Sokol <psyhoge@gmail.com>
2021-01-16 01:54:35 +0100

    implement get weather in date range on dispatcher server side

--- dispatcherserver/server.py
+++ dispatcherserver/server.py
@@ -1,8 +1,9 @@
 from errors import ModelServerException
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from http import HTTPStatus
 from urllib.error import URLError
+from datetime import date
 
 
 app = Flask(__name__)
@@ -45,15 +46,26 @@ def get_city_current_weather(city_id):
         app.logger.error(f'Error for city {city_id} - {ex.message}')
         return {'message': ex.message}, ex.http_code
     except Exception as ex:
-        app.logger.error(f'Internal error for city {city_id} - {ex.with_traceback()}')
+        app.logger.error(f'Internal error for city {city_id} - {str(ex)}')
         return {'message': 'Internal server error'}, HTTPStatus.INTERNAL_SERVER_ERROR
 
 
 @app.route('/cities/<int:city_id>/weather')
 def get_city_weather(city_id):
     try:
+        start_date = request.args.get(key='start_date', default=date.today(), type=date.fromisoformat)
+        end_date = request.args.get(key='end_date', default=date.today(), type=date.fromisoformat)
         model_proxy = get_model_server_proxy(city_id)
-        return jsonify(model_proxy.get_city_weather())
+        result = model_proxy.get_city_weather(start_date, end_date)
+        return jsonify(result)
+    except ValueError:
+        return {'message': 'Incorrect dates format'}, HTTPStatus.BAD_REQUEST
     except URLError as err:
         app.logger.error(f'Error for city {city_id} - {err.reason}')
         return {'message': err.reason}, HTTPStatus.INTERNAL_SERVER_ERROR
+    except ModelServerException as ex:
+        app.logger.error(f'Error for city {city_id} - {ex.message}')
+        return {'message': ex.message}, ex.http_code
+    except Exception as ex:
+        app.logger.error(f'Internal error for city {city_id} - {str(ex)}')
+        return {'message': 'Internal server error'}, HTTPStatus.INTERNAL_SERVER_ERROR
